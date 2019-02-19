d57c5258dee84f0a04fa504b5994ba3c0d3a97ca
Patrick Thomas <pwt5ca@virginia.edu>
2019-02-19 10:27:05 -0500

    Changed to now fully use search results.

--- src/Main.java
+++ src/Main.java
@@ -13,17 +13,29 @@ public class Main {
 
         System.out.println("AllSearch example.");
 
+        System.out.println("Currently available platforms:");
+        for (String s : AllSearch.allPlatforms) {
+            System.out.printf("\t%s\n", s);
+        }
+
         Scanner reader = new Scanner(System.in); // reading from command line
         AllSearch searcher = new AllSearch(); // create searcher
         List<SearchResult> resultsList; // list that results will be stored in
 
         while (true) {
-            System.out.print("\n\t>\t");
+            System.out.print("\nquery\t>\t");
             String s = reader.nextLine();
             if (s.equals("quit"))
                 break; // quit if search is "quit"
-            else
+            else {
                 resultsList = searcher.search(s); // get results and store them
+                for (SearchResult sr : resultsList) {
+                    System.out.printf("%s\n", sr.getMovieTitle());
+                    for (String platform : AllSearch.allPlatforms) {
+                        System.out.printf("\t%-12s\t%s\n", platform, sr.getSiteLink(platform));
+                    }
+                }
+            }
         }
 
         // once quit has been typed
